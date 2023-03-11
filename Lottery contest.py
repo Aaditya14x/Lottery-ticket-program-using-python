cd49675701676129c743c6cde5474cb6b457ef49
Aaditya dubey <115134350+Aaditya14x@users.noreply.github.com>
2023-03-11 22:20:59 +0530

    Add files via upload
    This is a lottery prize program which is made in python

---
+++ Lottery contest.py
@@ -0,0 +1,14 @@
+print("welcome to Dubey_ji lottery coupon contest")
+#Let the coupon having number 1904 or 1389 or 5630 will be aloted the exciting prize 
+# the range of ticket is from(1000 to 9999) in any combination 
+i = int(input("Enter Your lucky Coupon Number : "))
+LCN=1904 or 1389 or 5630
+if(i==1904):
+    print("CONGRATULATIONS \n You won a Iphone")
+elif(i==1389):
+    print("CONGRATULATIONS \n You won a Washing machine")
+elif(i==5630):
+    print("CONGRATULATIONS \n You won a smart TV")
+else:
+    print("Bad Lucky")
+    print("Please try again next time ")
